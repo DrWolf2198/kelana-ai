58bdd8b3e2520105fcefa465f15a858eb571b103
DrWolf2198 <dryudhistira86@gmail.com>
2026-08-10 20:10:48 +0700

    Create initial KelanaAI console app

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+def print_trip_summary(destination, days, budget, travel_style,):
+    print('=========================')
+    print('kelanaAI')
+    print('=========================')
+    print(f'Destination : {destination}')
+    print(f'Days        : {days}')
+    print(f'Budget      : {budget}')
+    print(f'Style       : {travel_style}')
+
+    #Call it with any trip
+print_trip_summary('Japan', 5, 1500, 'Family')
+print_trip_summary('Bali', 3, 800, 'Backpacker')
