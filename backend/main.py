317819e40c2079a3e242df490a723626e094b00b
DrWolf2198 <dryudhistira86@gmail.com>
2026-08-14 23:58:26 +0700

    Add recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,43 @@
+from services.trip_service import(
+    calculate_daily_budget,
+    get_recommended_place,
+    get_transportation_recommendation,
+    get_trip_category
+    )
+
+def print_destination(destination):
+    print('Your Destination')
+
+    index=0
+    while index < len(destination):
+        print(f'{index+1}, {destination[index]}')
+        index+=1
+    
+def recommended_place(destination):
+    print('Recommended Places')
+    print()
+
+    for destination in destination:
+        print(destination)
+
+        for place in get_recommended_place(destination):
+            print(f' - {place}')
+
+        print()
+
+
 def print_trip_summary(
     destination,
     days,
     budget,
+    travel_style,
     currency,
     travel_month,
     hotel_cost,
     transportation_cost,
     food_cost,
-    miscellaneous_cost
+    miscellaneous_cost,
+    transportation
     ):
 
     total_estimate_cost = (
@@ -20,7 +50,11 @@ def print_trip_summary(
     print('=========================')
     print('kelanaAI')
     print('=========================')
+
     print(f'Destination         : {destination}')
+    print(f'Days                : {days}')
+    print(f'Budget              : {budget}')
+    print(f'Style               : {travel_style}')
     print(f'Days                : {days}')
     print(f'Budget              : {budget} {currency}')
     print(f'Currency            : {currency}')
@@ -29,21 +63,24 @@ def print_trip_summary(
     print(f'Transportation cost : {transportation_cost}')
     print(f'Food cost           : {food_cost}')
     print(f'Miscellaneous cost  : {miscellaneous_cost}')
+    print(f'Recommended Transportation : {transportation}')
+    print_re
 
     if total_estimate_cost > budget:
-        print('Budget exceeded!')
+            print('Budget exceeded!')
 
     print()
 
-    #Call it with any trip
+#  Call it with any trip
 print_trip_summary(
-    'Fuji',
-    7,
-    200000.0,
-    'Yen',
-    'July',
-    70000.0,
-    30000.0,
-    40000.0,
-    10000.0
-     )
+['Japan','Korea'],
+7,
+300.0,
+'Backpacker',
+'USD',
+'July',
+100.0,
+30.0,
+40.0,
+10.0
+)
